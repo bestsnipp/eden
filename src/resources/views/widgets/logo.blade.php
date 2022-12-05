bac9a53078f7b1a3f8ce03b29a728f9b0abc6770
Debasish Gharami <debasishgharami12@gmail.com>
2022-12-05 15:10:23 +0530

    logo added in sidebar

--- src/resources/views/widgets/logo.blade.php
+++ src/resources/views/widgets/logo.blade.php
@@ -1,3 +1,126 @@
 <div class="text-center">
-    <h3>-- Logo --</h3>
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="w-full h-10"
+         viewBox="0 0 1196 231">
+        <defs>
+            <style>
+                .a {
+                    fill: #fc5f52;
+                    font-family: SourceSansPro-Regular, Source Sans Pro;
+                }
+
+                .a, .b {
+                    font-size: 184px;
+                }
+
+                .b {
+                    fill: #8b5cf6;
+                    font-family: SourceSansPro-Light, Source Sans Pro;
+                    font-weight: 300;
+                }
+
+                .c, .d, .e, .f, .g, .h, .i, .j, .k {
+                    fill-rule: evenodd;
+                }
+
+                .c {
+                    fill: url(#a);
+                }
+
+                .d, .h {
+                    opacity: 0.35;
+                }
+
+                .d {
+                    fill: url(#b);
+                }
+
+                .e {
+                    fill: url(#c);
+                }
+
+                .f {
+                    fill: #fa9bc7;
+                }
+
+                .f, .k {
+                    opacity: 0.5;
+                }
+
+                .g {
+                    fill: url(#d);
+                }
+
+                .h, .k {
+                    fill: #ffbcfe;
+                }
+
+                .i {
+                    fill: #300c50;
+                }
+
+                .i, .j {
+                    opacity: 0.2;
+                }
+
+                .j {
+                    fill: #4b0301;
+                }</style>
+            <linearGradient id="a" x1="0.008" y1="0.781" x2="1.589" y2="-0.122" gradientUnits="objectBoundingBox">
+                <stop offset="0" stop-color="#fa1477"/>
+                <stop offset="1" stop-color="#ff0"/>
+            </linearGradient>
+            <linearGradient id="b" x1="-0.397" y1="0.866" x2="1.084" y2="0.064" gradientUnits="objectBoundingBox">
+                <stop offset="0" stop-color="#f9fcba"/>
+                <stop offset="1" stop-color="#fa1477"/>
+            </linearGradient>
+            <linearGradient id="c" x1="-0.083" y1="0.987" x2="0.999" y2="-0.035" gradientUnits="objectBoundingBox">
+                <stop offset="0" stop-color="#9027e2"/>
+                <stop offset="1" stop-color="#fa1477"/>
+            </linearGradient>
+            <linearGradient id="d" x1="-0.683" y1="1.445" x2="1.079" y2="0.226" gradientUnits="objectBoundingBox">
+                <stop offset="0" stop-color="#8b5cf6"/>
+                <stop offset="1" stop-color="#c835f8"/>
+            </linearGradient>
+        </defs>
+        <g transform="translate(-244 -426)">
+            <text class="a" transform="translate(244 607)">
+                <tspan x="0" y="0">Laravel</tspan>
+            </text>
+            <text class="b" transform="translate(1060 607)">
+                <tspan x="0" y="0">Eden</tspan>
+            </text>
+            <g transform="translate(677.564 2227.799)">
+                <path class="c"
+                      d="M257.164,95.589,187.07,55.109a19.79,19.79,0,0,0-19.99-.114L98.1,94.861A19.819,19.819,0,0,0,86.89,112.71v81.175a19.819,19.819,0,0,0,10,17.134l70.123,40.466A19.747,19.747,0,0,0,187,251.6l68.909-39.823a19.848,19.848,0,0,0,11.209-17.863v-81.2a19.8,19.8,0,0,0-9.952-17.12Z"
+                      transform="translate(76.546 -1839.141)"/>
+                <path class="d"
+                      d="M86.89,162.249v-62.4a19.819,19.819,0,0,1,9.238-16.763c41.665-8.567,86.5,66.182,170.988,16.763l-19.8,78.719a19.819,19.819,0,0,1-15.235,14.636l-76.677,21.8a19.748,19.748,0,0,1-19.291-4.969Z"
+                      transform="translate(76.546 -1826.282)"/>
+                <path class="e"
+                      d="M86.89,127.732v48.062a19.819,19.819,0,0,0,10,17.134L167.008,233.4a19.748,19.748,0,0,0,19.99.114l68.909-39.823a19.848,19.848,0,0,0,11.209-17.863V94.62C190.568,169.712,123.8,88.68,86.89,127.732Z"
+                      transform="translate(76.546 -1821.051)"/>
+                <path class="f"
+                      d="M86.89,127.724V150.57c23.417-20.776,37.753-17.606,67.724-14.607,34.012,3.384,74.678,5.9,112.5-41.323C190.568,169.7,123.8,88.671,86.89,127.724Z"
+                      transform="translate(76.546 -1821.043)"/>
+                <path class="g"
+                      d="M150.589,153.049c-28.943,4.426-46.235,21.261-56.729,39.881L164.026,233.4a19.748,19.748,0,0,0,19.99.114l68.909-39.823a19.847,19.847,0,0,0,11.209-17.863V94.62C242.031,142.825,197.724,145.838,150.589,153.049Z"
+                      transform="translate(79.529 -1821.051)"/>
+                <path class="h"
+                      d="M150.589,153.062c-28.943,4.426-46.235,21.261-56.729,39.881L126.087,211.5c7.639-14.379,19.99-27.287,39.98-35.083,44.407-17.334,86.986-29.985,98.024-81.832C242.031,142.838,197.724,145.851,150.589,153.062Z"
+                      transform="translate(79.529 -1821.064)"/>
+                <path class="i"
+                      d="M149.4,145.281c-29.186,2.47-48.633,15.978-61.4,38.253a19.991,19.991,0,0,0,8.367,9.4c10.495-18.562,27.786-35.454,56.729-39.881,47.12-7.139,91.384-10.224,113.545-58.429C241.34,141.226,196.933,141.254,149.4,145.281Z"
+                      transform="translate(77.021 -1821.051)"/>
+                <path class="j"
+                      d="M86.89,112.026v15.707C123.8,88.68,190.568,169.712,267.116,94.62,186.384,162.43,126.713,80.484,86.89,112.026Z"
+                      transform="translate(76.546 -1821.051)"/>
+                <path class="k"
+                      d="M150.589,153.049c-28.943,4.426-46.235,21.261-56.729,39.881l1.628.928c10.338-18.348,27.344-34.883,55.759-39.552,47.048-7.739,91.313-11.237,112.888-59.685C242.031,142.825,197.724,145.838,150.589,153.049Z"
+                      transform="translate(79.529 -1821.051)"/>
+                <path class="f"
+                      d="M86.89,127.732v2.3c8.681-8.567,16.263-11.994,29.029-11.951,38.267.1,92.284,35.6,151.2-23.46C190.568,169.712,123.8,88.68,86.89,127.732Z"
+                      transform="translate(76.546 -1821.051)"/>
+            </g>
+        </g>
+    </svg>
 </div>
