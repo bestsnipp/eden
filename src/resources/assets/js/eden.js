f2617c635fde75a377ad12641c8fd260b7173adc
Debasish Gharami <debasishgharami12@gmail.com>
2022-12-18 04:05:48 +0530

    Toast Animation Added

--- src/resources/assets/js/eden.js
+++ src/resources/assets/js/eden.js
@@ -1,40 +1,5 @@
-// Import ApexChart
-//import ApexCharts from 'apexcharts';
-//window.ApexCharts = ApexCharts; // return apex chart
-
-// Import jQuery
-//import $ from "jquery";
-//window.jQuery = window.$ = $;
-
-// Import Select2
-//import select2 from 'select2'
-//window.select2 = select2;
-
-// Import FlatPicker
-// import flatpickr from "flatpickr";
-// window.flatpickr = flatpickr;
-
-// Import Trix
-//import trix from 'trix';
-
-// Import Pickr
-//import Pickr from '@simonwep/pickr';
-//window.Pickr = Pickr;
-
 import _ from 'lodash';
 window._ = _;
-
-if (undefined !== jQuery) {
-
-    if (undefined !== NiceScroll) {
-        $(function() {
-            //$("body").niceScroll();
-            //$("#sidebar").niceScroll();
-        });
-    }
-
-
-}
 
 window.addEventListener('alpine:init', function () {
 
--- src/resources/views/widgets/toasts.blade.php
+++ src/resources/views/widgets/toasts.blade.php
@@ -1,9 +1,9 @@
-<div class="fixed w-full md:w-2/12 flex flex-col overflow-auto z-[9999] {{ [
-        'top-right' => 'top-0 right-5',
-        'top-left' => 'top-0 left-5',
-        'bottom-right' => 'bottom-0 right-5',
-        'bottom-left' => 'bottom-0 left-5',
-        'top-center' => 'top-0 left-1/2 -translate-x-1/2',
+<div class="fixed w-full md:w-2/12 flex flex-col overflow-auto z-[999] empty:hidden pb-5 px-1 {{ [
+        'top-right' => 'top-1.5 right-2.5',
+        'top-left' => 'top-1.5 left-3',
+        'bottom-right' => 'bottom-0 right-2.5',
+        'bottom-left' => 'bottom-0 left-3',
+        'top-center' => 'top-1.5 left-1/2 -translate-x-1/2',
         'bottom-center' => 'bottom-0 left-1/2 -translate-x-1/2',
         'center-center' => 'top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2',
     ][config('eden.toast')] }}"
@@ -12,17 +12,27 @@
         toasts.push(event.detail)
     }">
     <template x-for="(toast, index) in toasts.reverse()" :key="toast.hash">
-        <div x-data="{show: false}" x-show="show" x-transition
-            class="mt-2 bg-white shadow-lg overflow-hidden rounded-md w-full relative"
-            x-init="$nextTick(() => show = true); setTimeout(() => {toasts.splice(index, 1)}, {{ config('eden.toast_duration', 5000) }});">
+        <div x-data="{show: false}"
+             x-show="show"
+             x-transition:enter="transition ease-out duration-300"
+             x-transition:enter-start="opacity-0 scale-75"
+             x-transition:enter-end="opacity-100 scale-100"
+             x-transition:leave="transition ease-in duration-75"
+             x-transition:leave-start="opacity-100 scale-100"
+             x-transition:leave-end="opacity-0 scale-75"
+             class="mt-2 bg-white shadow-lg overflow-hidden rounded-md w-full relative"
+            x-init="$nextTick(() => {
+                show = true;
+                setTimeout(() => {show = false}, {{ config('eden.toast_duration', 5000) }});
+
+                $watch('show', value => {
+                    value === false ? setTimeout(() => {toasts.splice(index, 1)}, 76) : '';
+                })
+            })">
             <div class="py-2 rounded-md px-3 flex items-center m-1 bg-slate-100" :class="toast.class">
-                <span class="scale-75">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-                    </svg>
-                </span>
+                {!! edenIcon('bell', 'scale-75') !!}
                 <h4 class="grow font-medium ml-2" x-text="toast.title">Normal</h4>
-                <a href="#" :class="closeClass[toast.type]" @click.prevent="toasts.splice(index, 1)">
+                <a href="#" :class="closeClass[toast.type]" @click.prevent="show = false">
                     {!! edenIcon('x-mark') !!}
                 </a>
             </div>
