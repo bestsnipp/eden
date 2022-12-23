b7b03626d53710332a377c5ee40e527fcfb6a685
Debasish Gharami <debasishgharami12@gmail.com>
2022-12-23 14:00:14 +0530

    Statistics Card Percentage Change Option now can be disabled

--- src/resources/views/metrics/statistic.blade.php
+++ src/resources/views/metrics/statistic.blade.php
@@ -2,25 +2,26 @@
     <div class="flex items-center">
         <div class="grow">
             <h2 class="font-bold text-2xl text-slate-600 mt-3 mb-3 flex dark:text-slate-200">{!! $valueLabel !!}</h2>
-            <div class="flex items-center">
-                @if($change > 0)
-                    <span class="text-green-500 mr-2">
-                        {!! edenIcon('trending-up') !!}
-                    </span>
-                @endif
-                @if($change < 0)
-                    <span class="text-red-500 mr-2">
-                        {!! edenIcon('trending-down') !!}
-                    </span>
-                @endif
-                @if($showPerntageChange)
-                <p class="text-slate-500 dark:text-slate-300">
-                    @if($change > 0)<span class="font-medium text-green-500">{{ $change }}%</span> Increased @endif
-                    @if($change < 0)<span class="font-medium text-red-500">{{ $change }}%</span> Decreased @endif
-                    @if($change == 0) No Value for Analysis @endif
-                </p>
-                @endif
-            </div>
+
+            @if($showPerntageChange)
+                <div class="flex items-center">
+                    @if($change > 0)
+                        <span class="text-green-500 mr-2">
+                            {!! edenIcon('trending-up') !!}
+                        </span>
+                    @endif
+                    @if($change < 0)
+                        <span class="text-red-500 mr-2">
+                            {!! edenIcon('trending-down') !!}
+                        </span>
+                    @endif
+                    <p class="text-slate-500 dark:text-slate-300">
+                        @if($change > 0)<span class="font-medium text-green-500">{{ $change }}%</span> Increased @endif
+                        @if($change < 0)<span class="font-medium text-red-500">{{ $change }}%</span> Decreased @endif
+                        @if($change == 0) No Value for Analysis @endif
+                    </p>
+                </div>
+            @endif
         </div>
         @if($showIcon)
             <div class="ml-3">
