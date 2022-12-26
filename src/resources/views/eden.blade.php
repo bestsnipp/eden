59718c8ccd844faa6580e61862add3b00b1c081e
Debasish Gharami <debasishgharami12@gmail.com>
2022-12-26 22:29:21 +0530

    Dashboard Grid Column responsive Completed

--- src/resources/views/eden.blade.php
+++ src/resources/views/eden.blade.php
@@ -3,7 +3,7 @@
 @section('header', $title)
 
 @section('content')
-    <div class="grid md:grid-cols-2 xl:grid-cols-4 gap-3 my-4">
+    <div class="flex flex-col md:grid md:grid-cols-2 xl:grid-cols-4 gap-3 my-4">
         @foreach($components as $component)
             @if($component->isAuthorizedToSee())
                 @livewire($component->component, $component->params)
