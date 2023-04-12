e8058ae5910fa48e2f9e83a346f66a44cc1ea277
Debasish Gharami <deb@cloudifyapps.com>
2023-04-12 16:00:37 +0530

    Image Preview lTrim added for left slash

--- src/resources/views/fields/row/image.blade.php
+++ src/resources/views/fields/row/image.blade.php
@@ -5,7 +5,7 @@
                 <div class="flex -space-x-2">
                     @foreach(array_slice($value, 0, 4) as $image)
                         <span class="relative inline-flex items-center justify-center w-10 text-white rounded-full">
-                            <img src="{{ asset($image) }}" alt="{{ $image }}" class="border-2 border-white rounded-full aspect-square object-cover hover:scale-[2] hover:z-10 transition-all transition-75" />
+                            <img src="{{\Illuminate\Support\Facades\Storage::disk($disk)->url($image) }}" alt="{{ $image }}" class="border-2 border-white rounded-full aspect-square object-cover hover:scale-[2] hover:z-10 transition-all transition-75" />
                         </span>
                     @endforeach
                     @if(count($value) > 4)
@@ -17,7 +17,7 @@
             </div>
         @else
             <span class="relative inline-flex items-center justify-center w-10 text-white rounded-full group">
-                <img src="{{ asset($value) }}" alt="{{ $value }}" class="border-2 border-white rounded-full aspect-square object-cover hover:scale-[2] hover:z-10 transition-all transition-75" />
+                <img src="{{ \Illuminate\Support\Facades\Storage::disk($disk)->url($value) }}" alt="{{ $value }}" class="border-2 border-white rounded-full aspect-square object-cover hover:scale-[2] hover:z-10 transition-all transition-75" />
             </span>
         @endif
     @endif
